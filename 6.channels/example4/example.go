86f55f3441c457212a8b5c32ad83c44adb304ad5
rizallomotif <rizal@lomotif.com>
2022-07-17 11:30:17 +0700

    chore: update doc for channels

--- 6.channels/example4/example.go
+++ 6.channels/example4/example.go
@@ -5,7 +5,12 @@ import "fmt"
 func UniDirectionalChannel() {
 	// you can create directional channel with this way
 	var dataStream = make(chan interface{})
+
+	// <- {channel}, artinya kita akan menerima value/func dari channel
+
 	var receivedStream <-chan interface{}
+
+	// {channel} <-, artinya kita akan mengirim value/func ke channel tersebut
 	var sendStream chan<- interface{}
 
 	receivedStream = dataStream
