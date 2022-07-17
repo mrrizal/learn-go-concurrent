b73be0c82016c1f1072ee8afdf534536a9a09545
rizallomotif <rizal@lomotif.com>
2022-07-17 11:17:00 +0700

    chore: update doc

--- pool/main.go
+++ pool/main.go
@@ -11,7 +11,7 @@ func main() {
 	/*
 		the idea of pool is to make object or instance reusable.
 
-		the main benefit to use pool is reduce memory usage make your program faster.
+		the main benefit to use pool is reduce memory usage and make your program faster.
 
 		it's becuase you no need to create much object or instance, if the object or instance is available at the pool,
 		just reuse it, if not then create the new one.
